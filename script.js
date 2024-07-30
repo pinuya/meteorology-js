b79ea9104d3f2c372a0f95f1c858c9164f297687
Tifany <tifsnunes@gmail.com>
2024-07-30 17:40:44 -0300

    generate a new api key and .gitignore

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelector(".busca").addEventListener("submit", (event) => {
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
       input
-    )}&appid=e940b3b011483c9924dfddc5dfd2f56e`;
+    )}&appid=${API_KEY}&units=metric&lang=pt_br`;
   }
 });
 
